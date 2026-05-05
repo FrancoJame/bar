c830047090761ac09e09827d3a8759cec88158c5
FrancoJame <mutebifrancis33@gmail.com>
2026-05-05 15:34:05 +0300

    Add files via upload

--- products/urls.py
+++ products/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('', views.ProductListView.as_view(), name='product_list'),
     path('add/', views.ProductCreateView.as_view(), name='product_add'),
     path('category/<int:category_id>/', views.ProductListView.as_view(), name='product_list_by_category'),
+    path('dashboard/', views.DashboardView.as_view(), name='dashboard'),
+    path('api/dashboard-data/', views.DashboardDataView.as_view(), name='dashboard_data'),
 ]
